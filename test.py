2b148ec82b3a05f3ef629ce256daceb700391383
Sasha-Frolova <only27191@gmail.com>
2023-06-15 18:23:58 +0300

    test.py created

---
+++ test.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+# 1 зайти на главную страницу
+# 2 выбрать поле Username
+# 3 ввести standard_user
+# 4 выбрать поле Password
+# 5 ввести secret_sauce
+# 6 нажать кнопку Login
+
+def get_driver():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    return driver
+
+def open_page(driver, URL):
+    driver.get(URL)
+
+def get_element_by_id(driver, locator):
+   return driver.find_element(By.ID, locator)
+
+def element_click(driver, locator):
+    element = get_element_by_id(driver, locator)
+    element.click()
+
+def element_send_keys(driver, locator, text):
+    element = get_element_by_id(driver, locator)
+    element.send_keys(text)
+
+def login(driver, name, password):
+    element_send_keys(driver, 'user-name', name)
+    element_send_keys(driver, 'password', password)
+    element_click(driver, 'login-button')
+
+URL = 'https://www.saucedemo.com/'
+LOGIN = 'standard_user'
+PASSWORD = 'secret_sauce'
+
+driver = get_driver()
+open_page(driver, URL)
+login(driver=driver, name=LOGIN, password=PASSWORD)
+
+driver.quit()
